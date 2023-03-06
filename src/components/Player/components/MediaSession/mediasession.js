35dcd672bafaa7b9eb73783cbf3ac7085b0b5bf1
Meridian <meridianpy@gmail.com>
2023-03-06 16:06:12 +0100

    Move mediasession audio

--- src/components/Player/components/MediaSession/mediasession.js
+++ src/components/Player/components/MediaSession/mediasession.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import { WebsocketContext } from '../../../WebSocket/websocket.js'
 import './mediasession.css'
-import nearSilence from '../../near-silence.mp3'
+import nearSilence from './near-silence.mp3'
 
 export function MediaSession({ track, paused }) {
   const websocket = useContext(WebsocketContext)
