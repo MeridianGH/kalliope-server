3df97f5492a0e0e41ff4fea17b47ab004503af5f
Meridian <meridianpy@gmail.com>
2024-05-11 21:29:41 +0200

    fix toasts and mediasession

--- src/contexts/toastContext.tsx
+++ src/contexts/toastContext.tsx
@@ -1,27 +1,33 @@
-import React, { createContext, ReactElement, useReducer } from 'react'
+import React, { createContext, ReactElement, useCallback, useReducer } from 'react'
 import { ToastContainer } from '../components/ToastContainer/toastContainer'
 import { ToastProps } from '../components/Toast/toast'
 
 type toastContextType = {
-  info: (message: ToastProps['message']) => number,
-  success: (message: ToastProps['message']) => number,
-  warn: (message: ToastProps['message']) => number,
-  error: (message: ToastProps['message']) => number,
-  persistent: (type: ToastProps['type'], message: ToastProps['message']) => number,
+  info: (message: ToastProps['message']) => void,
+  success: (message: ToastProps['message']) => void,
+  warn: (message: ToastProps['message']) => void,
+  error: (message: ToastProps['message']) => void,
+  persistent: (type: ToastProps['type'], message: ToastProps['message'], onDismiss?: ToastProps['onDismiss']) => void,
   remove: (id: ToastProps['id']) => void
 }
 
 export const ToastContext = createContext<toastContextType | undefined>(undefined)
 
-type toastReducerAddAction = { type: 'ADD', payload: ToastProps }
+type toastReducerAddAction = { type: 'ADD', payload: Omit<ToastProps, 'id'> }
 type toastReducerRemoveAction = { type: 'REMOVE', payload: number }
 type toastReducerActionType = toastReducerAddAction | toastReducerRemoveAction
 
 
 function toastReducer(state: { toasts: ToastProps[] }, action: toastReducerActionType) {
+  console.log(action)
+  let id: ToastProps['id']
   switch (action.type) {
     case 'ADD':
-      return { toasts: [...state.toasts, action.payload] }
+      id = Math.floor(Math.random() * 10000000)
+      while (state.toasts.map((toast) => toast.id).includes(id)) {
+        id = Math.floor(Math.random() * 10000000)
+      }
+      return { toasts: [...state.toasts, Object.assign(action.payload, { id: id })] }
     case 'REMOVE':
       return { toasts: state.toasts.filter((toast) => toast.id !== action.payload) }
   }
@@ -30,27 +36,30 @@ function toastReducer(state: { toasts: ToastProps[] }, action: toastReducerActio
 export function ToastProvider({ children }: { children: ReactElement }) {
   const [state, dispatch] = useReducer(toastReducer, { toasts: [] })
 
-  function addToast(type: ToastProps['type'], message: ToastProps['message'], persistent?: ToastProps['persistent'], onDismiss?: ToastProps['onDismiss']) {
-    let id = Math.floor(Math.random() * 10000000)
-    while (state.toasts.map((toast) => toast.id).includes(id)) {
-      id = Math.floor(Math.random() * 10000000)
-    }
-    dispatch({ type: 'ADD', payload: { id: id, type: type, message: message, persistent: persistent, onDismiss: onDismiss } })
-    return id
-  }
+  const addToast = useCallback((type: ToastProps['type'], message: ToastProps['message'], persistent?: ToastProps['persistent'], onDismiss?: ToastProps['onDismiss']) => {
+    dispatch({ type: 'ADD', payload: { type: type, message: message, persistent: persistent, onDismiss: onDismiss } })
+  }, [])
 
-  const info: toastContextType['info'] = (message) => addToast('info', message)
+  const info: toastContextType['info'] = useCallback((message) => {
+    addToast('info', message)
+  }, [addToast])
 
-  const success: toastContextType['success'] = (message) => addToast('success', message)
+  const success: toastContextType['success'] = useCallback((message) => {
+    addToast('success', message)
+  }, [addToast])
 
-  const warn: toastContextType['warn'] = (message) => addToast('warn', message)
+  const warn: toastContextType['warn'] = useCallback((message) => {
+    addToast('warn', message)
+  }, [addToast])
 
-  const error: toastContextType['error'] = (message) => addToast('error', message)
+  const error: toastContextType['error'] = useCallback((message) => {
+    addToast('error', message)
+  }, [addToast])
 
-  const persistent: toastContextType['persistent'] = (type, message) => {
-    if (state.toasts.filter((toast) => toast.message === message).length > 0) { return -1 }
-    return addToast(type, message, true)
-  }
+  const persistent: toastContextType['persistent'] = useCallback((type, message, onDismiss) => {
+    // if (state.toasts.filter((toast) => toast.message === message).length > 0) { return -1 }
+    addToast(type, message, true, onDismiss)
+  }, [addToast])
 
   const remove: toastContextType['remove'] = (id) => {
     dispatch({ type: 'REMOVE', payload: id })
--- src/hooks/mediaSessionHook.ts
+++ src/hooks/mediaSessionHook.ts
@@ -7,56 +7,70 @@ import nearSilence from '../assets/near-silence.mp3'
 export function useMediaSession(guildId?: string, track?: Track, paused?: boolean) {
   const [hasError, setHasError] = useState<boolean>(false)
   const webSocket = useContext(WebSocketContext)
-  const toasts = useToasts()
+  const { persistent, warn } = useToasts()
 
   const displayAlert = useCallback(() => {
     console.warn('Autoplay disabled!')
-    const toast = toasts.persistent(
+    persistent(
       'error',
-      'Autoplay disabled! Close this message to enable media notifications or enable autoplay for this website in your browser.'
+      'Autoplay disabled! Close this message to enable media notifications or enable autoplay for this website in your browser.',
+      () => { setHasError(false) }
     )
     return () => {
-      toasts.remove(toast)
       setHasError(false)
     }
-  }, [toasts])
+  }, [persistent])
 
   useEffect(() => {
-    if (hasError) { return displayAlert() }
-  }, [hasError])
-
-  useEffect(() => {
-    if (!('mediaSession' in navigator)) { return displayAlert() }
+    if (!('mediaSession' in navigator) || !track) { return }
+    if (hasError) {
+      displayAlert()
+      return
+    }
 
     const audio = new Audio(nearSilence)
     audio.volume = 0.00001
-    audio.play()
-      .catch(() => {
-        setHasError(true)
-      })
-      .then(() => {
-        setTimeout(() => audio.pause(), 100)
-      })
-  }, [displayAlert])
-  useEffect(() => {
-    function htmlDecode(input: string) { return new DOMParser().parseFromString(input, 'text/html').documentElement.textContent }
-    if ('mediaSession' in navigator) {
-      if (!webSocket || !guildId || !track) {
-        navigator.mediaSession.metadata = null
-        return
-      }
-      navigator.mediaSession.metadata = new MediaMetadata({
-        title: htmlDecode(track.info.title) ?? 'Unknown title',
-        artist: htmlDecode(track.info.author) ?? 'Unknown artist',
-        album: htmlDecode(track.info.author) ?? 'Unknown album',
-        artwork: [{ src: htmlDecode(track.info.artworkUrl ?? '') ?? '' }]
-      })
-      navigator.mediaSession.playbackState = paused ? 'paused' : 'playing'
-
-      navigator.mediaSession.setActionHandler('play', () => { webSocket.sendData('pause', { guildId: guildId }) })
-      navigator.mediaSession.setActionHandler('pause', () => { webSocket.sendData('pause', { guildId: guildId }) })
-      navigator.mediaSession.setActionHandler('nexttrack', () => { webSocket.sendData('skip', { guildId: guildId }) })
-      navigator.mediaSession.setActionHandler('previoustrack', () => { webSocket.sendData('previous', { guildId: guildId }) })
+    audio.loop = true
+    audio.addEventListener('canplaythrough', () => {
+      audio.play()
+        .then(() => {
+          console.info('MediaSession started.')
+          // setTimeout(() => audio.pause(), 500)
+        })
+        .catch(() => {
+          console.log('catch audio')
+          setHasError(true)
+          audio.remove()
+        })
+    })
+    return () => {
+      audio.loop = false
     }
-  }, [track, paused, webSocket, guildId])
+  }, [displayAlert, hasError])
+
+  useEffect(() => {
+    if (!('mediaSession' in navigator)) {
+      warn('Your browser does not support media notifications!')
+      return
+    }
+    if (!webSocket || !guildId || !track) {
+      navigator.mediaSession.metadata = null
+      return
+    }
+
+    const htmlDecode = (input: string) => new DOMParser().parseFromString(input, 'text/html').documentElement.textContent
+
+    navigator.mediaSession.metadata = new MediaMetadata({
+      title: htmlDecode(track.info.title) ?? 'Unknown title',
+      artist: htmlDecode(track.info.author) ?? 'Unknown artist',
+      album: htmlDecode(track.info.author) ?? 'Unknown album',
+      artwork: [{ src: htmlDecode(track.info.artworkUrl ?? '') ?? '' }]
+    })
+    navigator.mediaSession.playbackState = paused ? 'paused' : 'playing'
+
+    navigator.mediaSession.setActionHandler('play', () => { webSocket.sendData('pause', { guildId: guildId }) })
+    navigator.mediaSession.setActionHandler('pause', () => { webSocket.sendData('pause', { guildId: guildId }) })
+    navigator.mediaSession.setActionHandler('nexttrack', () => { webSocket.sendData('skip', { guildId: guildId }) })
+    navigator.mediaSession.setActionHandler('previoustrack', () => { webSocket.sendData('previous', { guildId: guildId }) })
+  }, [guildId, paused, track, warn, webSocket])
 }
